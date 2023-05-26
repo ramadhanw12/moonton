f49685230332d04e6a3a4a42534b36598bd6e085
moodboardsagi <whymind12@gmail.com>
2023-05-26 20:05:09 +0700

    faet: slicing Sign in and Sign up

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,26 @@ use Inertia\Inertia;
 |
 */
 
-Route::redirect('/', '/login');
+Route::redirect('/', '/prototype/login');
+
+Route::prefix('prototype')->name('prototype.')->group(function() {
+    Route::get('login', function() {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+
+    Route::get('register', function() {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
+});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
